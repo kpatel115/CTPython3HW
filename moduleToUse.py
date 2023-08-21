1ccc73c636f742eb953bb9d0247f55e4a87fd0fc
Karan Patel <83877493+kpatel115@users.noreply.github.com>
2023-08-21 18:12:27 -0500

    Uploaded module

---
+++ moduleToUse.py
@@ -0,0 +1,6 @@
+'''
+A module to use for CodingTemple Module 2 Python 3 HW
+Author: Karan Patel
+'''
+def greetUser(name):
+    print(f"Hello Sir/Madam {name}. It is a pleasure to meet you. Right this way now.")
